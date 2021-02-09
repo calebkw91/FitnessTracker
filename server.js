c46bb6af17e25479bcc49e52de44e724aa05d38c
calebkw <walke1291@gmail.com>
2021-02-08 18:59:58 -0600

    created server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(logger("dev"));
+
+app.use(express.static("public"));
+
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+})
